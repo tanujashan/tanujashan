26a4c632c5cd7997cec9a2e1149bcd7fa48d042b
tanujashan <43032009+tanujashan@users.noreply.github.com>
2018-09-20 15:19:53 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,5 @@
+a=int(input("enter value of a")) 
+b=int(input("enter value of b"))
+c=(a+b)
+print(c)
+
---
+++ lab3.py
@@ -0,0 +1,33 @@
+arthimatic operations in python
+
+>>> a=25
+>>> b=53
+>>> c=43
+>>> s=(a+b+c)
+>>> print(c)
+43
+>>> print(s)
+121
+>>> a=40
+>>> b=21
+>>> c=(a/b)
+>>> print(c)
+1
+>>> print(a and b)
+21
+>>> a=40
+>>> b=20
+>>> c=(a/b)
+>>> print(c)
+2
+>>> a=30
+>>> b=35
+>>> c=50
+>>> a>b
+False
+>>> b>a
+True
+>>> d=(a/b-c)
+>>> print(d)
+-50
+
